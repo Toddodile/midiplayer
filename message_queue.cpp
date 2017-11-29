c0b5480fe432213a90a97b4c2153f2874e8d57b3
todd5913 <todd5913@vt.edu>
2017-11-29 15:52:40 -0500

    added event and track files

--- message_queue.cpp
+++ message_queue.cpp
@@ -1,11 +1,35 @@
 #include "message_queue.hpp"
+#include "track.hpp"
 
 #include <queue>
 #include <mutex>
 
+class Message {
+public:
+	enum TYPE { PLAY, PAUSE, STOP, EXIT };
+	Message(Track track);
+
+	Message(TYPE message);
+
+	bool isPlay();
+
+	bool isPause();
+
+	bool isStop();
+
+	bool isExit();
+
+	Track getTrack();
+};
+
 class MessageQueue {
 public:
 	MessageQueue();
+
+
 private:
+
+
+
 	mutable std::mutex myMutex;
 };
